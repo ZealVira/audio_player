e87e65bdc15bd9f61572a3d39350715f1336e8b0
ZealVira <145832018+ZealVira@users.noreply.github.com>
2023-12-18 14:14:00 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,73 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+void main() => runApp(MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+    ));
+
+class HomePage extends StatefulWidget {
+  Home createState() => Home();
+}
+
+class Home extends State<HomePage> {
+  late VideoPlayerController _controller;
+  late Future<void> _videoplayer;
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset('assets/Shubhaarambh.mp3');
+    _videoplayer = _controller.initialize();
+    _controller.setLooping(true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Audio player'),
+      ),
+      body: FutureBuilder(
+        future: _videoplayer,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: VideoPlayer(_controller),
+              
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+      
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            if (_controller.value.isPlaying) {
+              _controller.pause();
+            } else {
+              _controller.play();
+            }
+          });
+        },
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
+    );
+  }
+}
 
